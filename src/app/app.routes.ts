a0cbdafd519d5606bda9ff608b8df7a69e03e235
Ramandeep Bhagat <rdbhagat999@gmail.com>
2024-10-20 18:56:29 +0530

    adds AdminGuard to users route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -30,7 +30,7 @@ export const routes: Routes = [
         path: 'users',
         loadComponent: () =>
           import('./pages/users/users.component').then((m) => m.UsersComponent),
-        canActivate: [authGuard],
+        canActivate: [authGuard, adminAccessGuard],
       },
       {
         path: 'posts',

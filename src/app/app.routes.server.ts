d6bc809d07ee7595e875585412971a3d63235a22
Ramandeep Bhagat <rdbhagat999@gmail.com>
2025-04-15 02:31:38 +0530

    updated code

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,9 +1,55 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { inject } from '@angular/core';
+import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
+import { UserService } from './shared/services/user.service';
+import { PostService } from './shared/services/post.service';
+import { ProductService } from './shared/services/product.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'users/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(UserService);
+      const usersResp = await service.getAll();
+      const user = usersResp.map((user) => ({
+        id: `${user.id}`,
+      }));
+
+      return user ? user : [];
+    },
+  },
+  {
+    path: 'posts/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(PostService);
+      const postsResp = await service.getAll();
+      const posts = postsResp.map((user) => ({
+        id: `${user.id}`,
+      }));
+
+      return posts ? posts : [];
+    },
+  },
+  {
+    path: 'products/:id',
+    renderMode: RenderMode.Prerender,
+    fallback: PrerenderFallback.Server,
+    async getPrerenderParams() {
+      const service = inject(ProductService);
+      const productsResp = await service.getAll();
+      const products = productsResp.map((product) => ({
+        id: `${product.id}`,
+      }));
+
+      return products ? products : [];
+    },
   },
   {
     path: '**',
